d8ba423a720cba33bdb32853b2d5aa58f3b3ebf2
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2026-05-06 17:29:14 +0700

    reset

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { UserModule } from "./user/user.module";
 import { AuthModule } from "./auth/auth.module";
-import { CustomerModule } from './customer/customer.module';
+import { CustomerModule } from "./customer/customer.module";
 
 @Module({
   imports: [UserModule, AuthModule, CustomerModule],
--- src/user/user.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
-
-@Injectable()
-export class UserService {
-  constructor(private readonly prisma: PrismaClient) {}
-}
