1fb17bc967c6f0a3b686fd0c11890073a1efa92c
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2026-05-06 11:27:57 +0700

    update prisma for db

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import helmet from "helmet";
+import "dotenv/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(helmet());
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.CLIENT_URL
+        : "localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  });
+  app.setGlobalPrefix("api/v1");
+  await app.listen(process.env.PORT ?? 4000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error("Failed to start server:", err);
+  process.exit(1);
+});
